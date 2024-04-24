e9767abe2082d1f1ce4197f1a43c28add58d6769
angstar <ysangstar@gmail.com>
2024-04-24 16:17:15 +0900

    style edit

--- src/page/Detailstory.vue
+++ src/page/Detailstory.vue
@@ -1,37 +1,90 @@
 <template>
-    <div>
-      <h2>{{ $route.params.title }}</h2>
-      <p>{{ $route.params.content }}</p>
-      <p class="text-muted">{{ $route.params.sendtime }}</p>
-      <hr class="my-4">
-      <div class="row g-2">
-        <div class="col-auto ignore-auto-margin">
-          <button @click="fnList" class="btn btn-outline-dark">목록</button>
-          <button class="btn btn-outline-dark">수정</button>
-          <button class="btn btn-outline-dark">삭제</button>
-        </div>
-      </div>
+  <div class="container">
+    <h2>{{ $route.params.title }}</h2>
+    <p>{{ $route.params.content }}</p>
+    <p class="text-muted">{{ $route.params.sendtime }}</p>
+    <hr class="my-4">
+    <div class="button-wrap">
+      <button @click="fnList" class="btn cancel">목록</button>
+      <button class="btn edit">수정</button>
+      <button class="btn delete">삭제</button>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-        return {
-         posts: [],
-         memberNo: 1,
-        };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+      memberNo: 1,
+    };
+  },
+  methods: {
+    fnList() { // 리스트 화면으로 이동
+      this.$router.push('/story');
     },
-    methods: {
-        fnList(){ //리스트 화면으로 이동
-            this.$router.push('/story');
-        },
-        
-
-    }
   }
-  </script>
-  
-  <style>
+};
+</script>
 
-  </style>
+<style scoped>
+.container {
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+}
+
+h2 {
+  color: #43b984;
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+p {
+  font-size: 16px;
+  line-height: 1.6;
+  margin-bottom: 10px;
+}
+
+.text-muted {
+  color: #888;
+}
+
+hr {
+  border: none;
+  border-top: 1px solid #ccc;
+  margin: 20px 0;
+}
+
+.button-wrap {
+  text-align: right;
+}
+
+.button-wrap .btn {
+  padding: 10px 20px;
+  margin-left: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.button-wrap .btn.cancel {
+  background-color: #ccc;
+  color: #333;
+}
+
+.button-wrap .btn.edit {
+  background-color: #f0ad4e;
+  color: #fff;
+}
+
+.button-wrap .btn.delete {
+  background-color: #d9534f;
+  color: #fff;
+}
+</style>
--- src/page/Newstory.vue
+++ src/page/Newstory.vue
@@ -1,106 +1,129 @@
 <template>
-  <div>
-    <h2>게시글 등록</h2>
-        <div class="AddWrap">
-            <form>
-                <table class="tbAdd">
-                    <colgroup>
-                        <col width="15%" />
-                        <col width="*" />
-                    </colgroup>
-                    <tr>
-                        <th>제목</th>
-                        <td><input type="text" v-model="subject" ref="subject" /></td>
-                    </tr>
-                    <tr>
-                        <th>내용</th>
-                        <td><textarea v-model="cont" ref="cont"></textarea></td>
-                    </tr>
-                </table>
-            </form>
-        </div>
-        <div class="btnWrap">
-            <a href="javascript:;" @click="fnList" class="btn">목록</a>
-            <a href="javascript:;" @click="fnAddProc" class="btnAdd btn">등록</a>
-        </div>
+    <div class="container">
+      <h2 class="title">게시글 등록</h2>
+      <div class="form-wrap">
+        <form>
+          <div class="form-group">
+            <label for="subject">제목</label><br>
+            <input type="text" v-model="post.title" id="subject" placeholder="제목을 입력하세요" />
+          </div>
+          <div class="form-group">
+            <label for="content">내용</label><br>
+            <textarea v-model="post.content" id="content" placeholder="내용을 입력하세요" class="content-textarea"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="button-wrap">
+        <button @click="fnList" class="btn cancel">목록</button>
+        <button @click="fnAddProc" class="btn submit">등록</button>
+      </div>
     </div>
-</template>
-<script>
-import axios from 'axios';
-export default {
-  name: "NewStory",
-  data(){
-  },
-  setup() {
-    return {
-     
-      post: {
-        title: "",
-        content: "",
-      },
-    };
-  },
-  components: {},
-  methods: {
-    fnList(){ //리스트 화면으로 이동
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: "NewStory",
+    data() {
+      return {
+        post: {
+          title: "",
+          content: "",
+        },
+      };
+    },
+    methods: {
+      fnList() { // 리스트 화면으로 이동
         this.$router.push('/story');
-    }
-        ,fnAddProc() { //등록 프로세스
-            if(!this.subject) { //제목이 없다면 값을 입력하라고 알려준다.
-                alert("제목을 입력해 주세요");
-                this.$refs.subject.focus(); //방식으로 선택자를 찾는다.
-                return;
-            }
-
-            //backend로 전송될 POST 데이터
-            let form = {} 
-            form.title = this.subject;
-            form.content = this.cont;
-            form.member_no =this.$store.getters.getUserInfo[0].member_no;
-            form.name = this.$store.getters.getUserInfo[0].name;
-            
-                 
-            let url = 'http://localhost:3000/Newstory';
-            axios.post(url,form)
-            .then(res=>{
-              console.log(res);
-              console.log(res.data.mes);
-                if(res.data.mes == 0) {
-                    alert('게시물 등록중 오류가 발생하였습니다.');
-                } else {
-                    alert('정상적으로 등록되었습니다.');
-                    this.fnList();
-                }
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
+      },
+      fnAddProc() { // 등록 프로세스
+        if (!this.post.title) { // 제목이 없을 경우
+          alert("제목을 입력해 주세요");
+          return;
         }
-    }
-};
-</script>
-<style scoped>
-    .tbAdd{border-top:1px solid #888;}
-    .tbAdd th, .tbAdd td{border-bottom:1px solid #eee; padding:5px 0;}
-    .tbAdd td{padding:10px 10px; box-sizing:border-box;}
-    .tbAdd td input{width:100%; min-height:30px; box-sizing:border-box; padding:0 10px;}
-    .tbAdd td textarea{width:100%; min-height:300px; padding:10px; box-sizing:border-box;}
-    .btnWrap{text-align:center; margin:20px 0 0 0;}
-    .btnWrap a{margin:0 10px;}
-    .btnAdd {background:#43b984}
-    .btnDelete{background:#f00;}
-html,body{padding:0; margin:0;}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2C3E50;
-}
-h1{color:#43b984;}
-table{width:100%; border-collapse:collapse;}
-.wrap{width:100%;}
-.container{width:800px; margin:0 auto;}
-a{text-decoration:none;}
-.btn{padding:10px; background:#34445c; color:#fff;}
-</style>
+  
+        // POST 데이터 설정
+        const form = {
+          title: this.post.title,
+          content: this.post.content,
+          member_no: this.$store.getters.getUserInfo[0].member_no,
+          name: this.$store.getters.getUserInfo[0].name,
+        };
+  
+        // POST 요청
+        const url = 'http://localhost:3000/Newstory';
+        axios.post(url, form)
+          .then(res => {
+            if (res.data.mes == 0) {
+              alert('게시물 등록 중 오류가 발생하였습니다.');
+            } else {
+              alert('정상적으로 등록되었습니다.');
+              this.fnList();
+            }
+          })
+          .catch(err => {
+            console.error(err);
+            alert('게시물 등록 중 오류가 발생하였습니다.');
+          });
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .container {
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+    text-align: left;
+  }
+  
+  .title {
+    color: #43b984;
+    margin-bottom: 20px;
+  }
+  
+  .form-wrap {
+    margin-bottom: 20px;
+  }
+  
+  .form-group {
+    margin-bottom: 15px;
+  }
+  
+  label {
+    display: block;
+    font-weight: bold;
+  }
+  
+  input[type="text"],
+  textarea {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+  
+  textarea.content-textarea {
+    min-height: 300px; /* 내용 textarea의 최소 높이 설정 */
+  }
+  
+  button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  .button-wrap button.cancel {
+    background-color: #ccc;
+    margin-right: 10px;
+  }
+  
+  .button-wrap button.submit {
+    background-color: #43b984;
+    color: #fff;
+  }
+  </style>
+  
