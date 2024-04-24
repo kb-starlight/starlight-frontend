421b7a4ba83fce69bce864fce9ca41d6ec3f8224
cse <xingen7@naver.com>
2024-04-24 18:10:52 +0900

    진행중인 제휴업체 수정 (주석버전)

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -96,6 +96,19 @@
               description="Description3"
             />
           </div>
+            <!-- <div class="cards-container" style="margin-top: 30px">
+              <a href="https://happybean.naver.com/campaign/dreambase"><img src="@/assets/제휴업체1.png"  /> </a> 
+              <a href="https://happybean.naver.com/template/campaign/2405withcong"><img src="@/assets/제휴업체2.png"  /> </a> 
+              <a href="https://happybean.naver.com/campaign/2024greenweek"><img src="@/assets/제휴업체3.png"  /> </a> 
+              <a href="https://happybean.naver.com/campaign/earthday2024"><img src="@/assets/제휴업체4.png"  /> </a> 
+            </div>
+            <div class="cards-container">
+              <a href="https://happybean.naver.com/project/2929"><img src="@/assets/제휴업체5.png"  /> </a> 
+              <a href="https://happybean.naver.com/project/2905"><img src="@/assets/제휴업체6.png"  /> </a> 
+              <a href="https://happybean.naver.com/project/tmoney"><img src="@/assets/제휴업체7.png"  /> </a> 
+              <a href="https://happybean.naver.com/campaign/greener2024"><img src="@/assets/제휴업체8.png"  /> </a> 
+            </div> -->
+          
         </div>
       </div>
     </div>
