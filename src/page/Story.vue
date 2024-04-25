3b1399c14e23d8fbb1b0033e1a41ce69528a42c6
cse <xingen7@naver.com>
2024-04-25 15:41:08 +0900

    story 랭킹 추가

--- src/page/Story.vue
+++ src/page/Story.vue
@@ -4,27 +4,33 @@
   </div>
   <div class="banner" id="banner">
     <div class="cards-container">
-      <preview-card
-        image="https://happybean-phinf.pstatic.net/20240409_243/1712656139366lY24E_PNG/4.%25A1%25E1%25BC%25E1%25A9%25E1%25A8%25E1%25A5%25E1.png?type=a360"
-        label="집이 무너졌어요"
-        description="Description1"
-      />
-      <preview-card
-        image="https://happybean-phinf.pstatic.net/20240409_84/1712656161293toLYm_PNG/5.%25A6%25E1%25AB%25E1%25A6%25E1%25B3%25E1%25A6%25E1%25B5%25E1%25B3%25E1.png?type=a360"
-        label="rank2"
-        description="Description2"
-      />
-      <preview-card
-        image="https://happybean-phinf.pstatic.net/20240409_242/1712656181657O4qMe_PNG/6.%25A6%25E1%25A5%25E1%25B3%25E1%25A6%25E1%25B5%25E1%25B3%25E1%25A9%25E1%25B5%25E1%25A1%25E1%25B3%25E1.png?type=a360"
-        label="rank3"
-        description="Description3"
-      />
-      <preview-card
-        image="https://happybean-phinf.pstatic.net/20240409_242/1712656181657O4qMe_PNG/6.%25A6%25E1%25A5%25E1%25B3%25E1%25A6%25E1%25B5%25E1%25B3%25E1%25A9%25E1%25B5%25E1%25A1%25E1%25B3%25E1.png?type=a360"
-        label="rank4"
-        description="Description4"
-      />
-    </div>
+            <a class="href" href="http://localhost:8080/Detailstory/%EC%84%A4%EC%9D%84%20%EB%A7%9E%EC%95%84%20%ED%95%A8%EA%BB%98%20%EB%A7%8C%EB%93%A0%20%EC%9D%B8%EC%A0%88%EB%AF%B8/%ED%95%A8%EA%BB%98%20%EB%A7%8C%EB%93%A0%20%EC%9D%B8%EC%A0%88%EB%AF%B8%EB%A1%9C%20%EC%A0%84%EB%8B%AC%ED%95%98%EB%8A%94%20%EB%94%B0%EC%8A%A4%ED%95%9C%20%EB%A7%88%EC%9D%8C.%20%EA%B3%A0%EC%95%84%EC%9B%90%EC%97%90%EB%8F%84%201%EB%85%84%EC%97%90%20%ED%95%9C%20%EB%B2%88%EC%94%A9%20%EC%B6%95%EC%A0%9C%EA%B0%80%20%EC%97%B4%EB%A6%BD%EB%8B%88%EB%8B%A4.%20%EB%B0%94%EB%A1%9C%20%EC%9D%B8%EC%A0%88%EB%AF%B8%20%EB%A7%8C%EB%93%9C%EB%8A%94%20%EB%82%A0%EC%9E%85%EB%8B%88%EB%8B%A4.%20%EC%95%84%EC%9D%B4%EB%93%A4%EC%9D%B4%20%ED%95%A8%EA%BB%98%20%EC%A7%81%EC%A0%91%20%EB%A7%8C%EB%93%A0%20%EC%9D%B8%EC%A0%88%EB%AF%B8%EB%A5%BC%20%EB%82%98%EB%88%A0%EB%A8%B9%EC%9C%BC%EB%A9%B0%20%EC%9B%83%EA%B3%A0%20%EC%9E%88%EC%8A%B5%EB%8B%88%EB%8B%A4.%20%EC%84%B1%EC%9D%80%EC%9D%B4%EB%8A%94%20%EC%98%A4%EB%9E%9C%EB%A7%8C%EC%97%90%20%EC%9D%B8%EC%A0%88%EB%AF%B8%EB%A5%BC%20%EB%A8%B9%EC%96%B4%20%EA%B8%B0%EB%B6%84%EC%9D%B4%20%EC%A2%8B%EC%8A%B5%EB%8B%88%EB%8B%A4.%20%EB%8B%B9%EC%8B%A0%EC%9D%98%20%EB%94%B0%EB%9C%BB%ED%95%9C%20%EB%82%98%EB%88%94%EC%9C%BC%EB%A1%9C%20%EC%95%84%EC%9D%B4%EB%93%A4%EC%9D%98%20%EC%9B%83%EC%9D%8C%EC%9D%84%20%EC%A7%80%EC%BC%9C%EC%A3%BC%EC%84%B8%EC%9A%94./2024-04-25T06:06:38.000Z/106/10000">
+              <preview-card
+              image="https://happybean-phinf.pstatic.net/20240409_243/1712656139366lY24E_PNG/4.%25A1%25E1%25BC%25E1%25A9%25E1%25A8%25E1%25A5%25E1.png?type=a360"
+              label="설을 맞아 함께 만든 인절미"
+              description=" 함께 만든 인절미로 전달하는 따스한 마음을 함께 나눠요"
+            /></a>
+            <a class="href" href="http://localhost:8080/Detailstory/%EC%9B%90%EC%9E%A5%EB%8B%98%EA%BB%98%20%EC%A0%84%ED%95%98%EB%8A%94%20%EA%B0%90%EC%82%AC%EC%9D%98%20%EB%A7%88%EC%9D%8C/%EC%8A%A4%EC%8A%B9%EC%9D%98%20%EB%82%A0%EC%9D%84%20%EB%A7%9E%EC%95%84%20%EC%B9%B4%EB%84%A4%EC%9D%B4%EC%85%98%EC%9D%84%20%EB%8B%AC%EC%95%84%EB%93%9C%EB%A6%AC%EA%B3%A0%20%EC%8B%B6%EC%97%88%EC%96%B4%EC%9A%94.%20%0A%EC%84%B1%EC%9D%80%EC%9D%B4%EB%8A%94%20%EC%98%A4%EB%8A%98%EB%8F%84%20%EC%82%B0%EC%9D%84%20%EB%92%A4%EC%A7%91%EB%8B%88%EB%8B%A4.%20%EC%B9%B4%EB%84%A4%EC%9D%B4%EC%85%98%EC%9D%84%20%EC%B0%BE%EA%B8%B0%20%EC%9C%84%ED%95%B4%EC%84%9C%EC%9D%B4%EC%A3%A0...%20%0A%ED%82%A4%EC%9B%8C%EC%A3%BC%EC%8B%9C%EB%8A%94%20%EA%B3%A0%EC%95%84%EC%9B%90%20%EC%9B%90%EC%9E%A5%EB%8B%98%EA%BB%98%20%EA%B0%90%EC%82%AC%EC%9D%98%20%EB%A7%88%EC%9D%8C%EC%9D%84%20%EC%A0%84%ED%95%98%EA%B3%A0%20%EC%8B%B6%EC%8A%B5%EB%8B%88%EB%8B%A4./2024-04-25T06:11:17.000Z/107/10000">
+            <preview-card
+              image="https://happybean-phinf.pstatic.net/20240409_84/1712656161293toLYm_PNG/5.%25A6%25E1%25AB%25E1%25A6%25E1%25B3%25E1%25A6%25E1%25B5%25E1%25B3%25E1.png?type=a360"
+              label="원장님께 전하는 감사의 마음"
+              description="스승의 날을 맞아 카네이션을 달아드리고 싶었어요"
+            />
+            </a>
+            <a class="href" href="http://localhost:8080/Detailstory/%EC%8B%9C%EA%B0%81%20%EC%9E%A5%EC%95%A0%EC%9D%B8%EB%93%A4%EC%9D%B4%20%EB%A7%8C%EB%93%A0%20%EA%B0%80%EB%B0%A9/%EC%A1%B0%EA%B8%88%20%EB%8A%90%EB%A6%AC%EB%8D%94%EB%9D%BC%EB%8F%84,%20%EC%A7%81%EC%A0%91%20%EB%A7%8C%EB%93%A0%20%EB%9D%BC%ED%83%84%20%EA%B0%80%EB%B0%A9.%20%EC%8A%A4%ED%83%80%EB%9D%BC%EC%9D%B4%ED%8A%B8%20%EA%B0%80%EB%B0%A9%EA%B3%BC%20%ED%95%A8%EA%BB%98%20%EC%82%AC%EB%9E%91%EC%9D%98%20%EC%8B%A4%EC%B2%9C%20%ED%95%A8%EA%BB%98%ED%95%B4%EC%9A%94%0A%EC%8B%9C%EA%B0%81%20%EC%9E%A5%EC%95%A0%EC%9D%B8%20%EC%9D%BC%EC%9E%90%EB%A6%AC%20%EC%A7%80%EC%9B%90%20%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%A8%EC%9C%BC%EB%A1%9C%20%EC%A7%84%ED%96%89%EB%90%98%EB%8A%94%20%EC%8A%A4%ED%83%80%EB%9D%BC%EC%9D%B4%ED%8A%B8%20%EA%B0%80%EB%B0%A9.%20%0A%EC%A1%B0%EA%B8%88%20%EC%84%9C%ED%88%B4%EB%8D%94%EB%9D%BC%EB%8F%84%20%ED%95%A8%EA%BB%98%ED%95%B4%EC%A3%BC%EC%84%B8%EC%9A%94.%0A%EC%88%98%EC%9D%B5%EA%B8%88%EC%9D%80%20%EC%A0%84%EC%95%A1%20%EC%8B%9C%EA%B0%81%EC%9E%A5%EC%95%A0%EC%9D%B8%20%EB%B3%B4%ED%98%B8%EC%9E%AC%EB%8B%A8%20%EB%85%B8%EB%9E%80%ED%8E%84%EC%9D%B4%EC%97%90%20%EA%B8%B0%EB%B6%80%EB%90%A9%EB%8B%88%EB%8B%A4./2024-04-25T06:14:22.000Z/108/10000">
+            <preview-card
+              image="https://happybean-phinf.pstatic.net/20240409_242/1712656181657O4qMe_PNG/6.%25A6%25E1%25A5%25E1%25B3%25E1%25A6%25E1%25B5%25E1%25B3%25E1%25A9%25E1%25B5%25E1%25A1%25E1%25B3%25E1.png?type=a360"
+              label="시각 장애인들이 만든 가방"
+              description="조금 느리더라도, 직접 만든 라탄 가방. 스타라이트 가방과 함께 사랑의 실천 함께해요"
+            />
+            </a>
+            <a class="href" href="http://localhost:8080/Detailstory/%EC%9A%94%EB%A6%AC%EC%82%AC%EA%B0%80%20%EB%90%98%EA%B3%A0%EC%8B%B6%EC%9D%80%20%EC%84%B8%EC%95%99%EC%9D%B4/%EC%9A%94%EB%A6%AC%EC%82%AC%EA%B0%80%20%EA%BF%88%EC%9D%B4%EC%97%88%EB%8D%98%20%EC%84%B8%EC%95%99%EC%9D%B4.%20%EA%BF%88%EB%B3%B4%EB%8B%A8%20%ED%98%84%EC%8B%A4%EC%9D%B4%20%EA%B0%80%EA%B9%9D%EA%B8%B0%EC%97%90%20%EC%98%A4%EB%8A%98%EB%8F%84%20%ED%98%84%EC%8B%A4%EC%9D%84%20%EC%82%B4%EC%95%84%EA%B0%80%EA%B3%A0%20%EC%9E%88%EC%8A%B5%EB%8B%88%EB%8B%A4.%20%EC%95%84%ED%94%88%20%EB%8F%99%EC%83%9D%EC%9D%84%20%EB%8F%8C%EB%B3%B4%EA%B8%B0%20%EC%9C%84%ED%95%B4%20%EA%BF%88%EC%9D%80%20%EC%BB%A4%EB%85%95%20%ED%95%99%EA%B5%90%20%EB%8B%A4%EB%8B%88%EA%B8%B0%EB%8F%84%20%EB%B2%85%EC%B0%AC%20%EC%84%B8%EC%95%99%EC%9D%B4%EC%9D%98%20%EA%BF%88%EC%9D%84%20%EC%9D%B4%EB%A4%84%EC%A3%BC%EC%84%B8%EC%9A%94.%20/2024-04-25T06:17:08.000Z/109/10000">
+            <preview-card
+              image="https://happybean-phinf.pstatic.net/20240409_239/1712656062181JJiqi_PNG/9.%25A1%25E1%25AC%25E1%25A9%25E1.png?type=a360"
+              label="요리사가 되고싶은 세앙이"
+              description="세앙이의 꿈을 이뤄주세요. 여러분의 손길이 한 아이의 인생을 바꿀 수 있습니다."
+            /></a>
+          </div>
   </div>
   <div class="contents">
     <h3>진행중인 사연</h3>
@@ -204,5 +210,8 @@ h3 {
   overflow: unset;
   z-index: 1;
   position: relative;
+}
+.href{
+  text-decoration: none;
 }
 </style>
